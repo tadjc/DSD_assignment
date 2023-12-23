1a975d9baa463fce9a6f03d18e5de34afef678f6
tadjc <janathdilan@yahoo.com>
2023-12-23 07:40:25 +0530

    Intergration have some issues

--- data_path_test_1.v
+++ data_path_test_1.v
@@ -1,11 +1,31 @@
-module data_path_test_1(sel1,sel2,sel3,sel4,mux1,mux2,data_in1,data_in2,data_in3,data_in4,clk,hsel_1,hsel_2,hsel_3);
+module data_path_test_1(sel1,sel2,sel3,sel4,mux1,mux2,data_in1,data_in2,data_in3,data_in4,clk,hsel_1,hsel_2,hsel_3,
+                                hrdata,hreadyout,hresponse,hrdata_1,hrdata_2,hrdata_3,hreadyout_1,hreadyout_2,hreadyout_3,hresp_1,hresp_2,hresp_3,sel);
 
     input sel1,sel2,sel3,sel4,mux1,mux2,clk;
     input [15:0] data_in1, data_in2;
     input [31:0] data_in3, data_in4;
+
+
+    input [31:0] hrdata_1,hrdata_2,hrdata_3;
+    input hreadyout_1,hreadyout_2,hreadyout_3,hresp_1,hresp_2,hresp_3;
+    input [2:0] sel;
+
+    output [31:0] hrdata;
+    output hreadyout,hresponse;
+
+
     output hsel_1,hsel_2,hsel_3;
+    
+    
+
     wire [15:0] z,x,y;
     wire [31:0] p,q,r;
+    
+//This part has some errors
+    assign sel[0] = test1_dec(hsel_1);
+    assign sel[1] = hsel_2;
+    assign sel[2] = hsel_3;
+   
 
     PIPO HADDR1 (x,data_in1,sel1,clk);
     PIPO HADDR2 (y,data_in2,sel2,clk);
@@ -15,8 +35,9 @@ module data_path_test_1(sel1,sel2,sel3,sel4,mux1,mux2,data_in1,data_in2,data_in3
 
     MUX addressmux (z,x,y,mux1);
     MUXD datamux (r,p,q,mux2);
-
-
     decoder test1_dec (hsel_1,hsel_2,hsel_3,z);
+
+    MUXR readmux (hrdata,hreadyout,hresponse,hrdata_1,hrdata_2,hrdata_3,hreadyout_1,hreadyout_2,hreadyout_3,hresp_1,hresp_2,hresp_3,sel);
+
     
 endmodule
--- test1_tb.v
+++ test1_tb.v
@@ -3,6 +3,7 @@
 `include "data_path_test_1.v"
 `include "MUX.v"
 `include "MUXD.v"
+`include "MUXR.v"
 `include "PIPO.v"
 `include "PIPOD.v"
 `include "decoder.v"
@@ -12,10 +13,18 @@ module test1_tb;
 reg [15:0] data_in1,data_in2;
 reg [31:0] data_in3,data_in4;
 
+
+reg [31:0] hrdata_1,hrdata_2,hrdata_3;
+reg hreadyout_1,hreadyout_2,hreadyout_3,hresp_1,hresp_2,hresp_3;
+
 reg clk, start;
 wire done;
 
-data_path_test_1 dpt1 (sel1,sel2,sel3,sel4,mux1,mux2,data_in1,data_in2,data_in3,data_in4,clk,hsel_1,hsel_2,hsel_3);
+wire [31:0] hrdata;
+wire hreadyout,hresponse;
+
+data_path_test_1 dpt1 (sel1,sel2,sel3,sel4,mux1,mux2,data_in1,data_in2,data_in3,data_in4,clk,hsel_1,hsel_2,hsel_3,
+                        hrdata,hreadyout,hresponse,hrdata_1,hrdata_2,hrdata_3,hreadyout_1,hreadyout_2,hreadyout_3,hresp_1,hresp_2,hresp_3,sel);
 control_test_1 cpt1(sel1,sel2,sel3,sel4,start,done,clk,mux1,mux2);
 
 initial
@@ -32,9 +41,21 @@ initial
 
         #1
         data_in1 = 16'b0000000000001000; #1;
-        data_in2 = 16'b0100000000001000; #1;
+        data_in2 = 16'b0010000000001000; #1;
         data_in3 = 10; #1;
         data_in4 = 20; #1;
+
+        hrdata_1  = 30;#1;
+        hrdata_2  = 40;#1;
+        hrdata_3  = 50;#1;
+
+        hreadyout_1 = 1'b1;#1;
+        hreadyout_2 = 1'b0;#1;
+        hreadyout_3 = 1'b0;#1;
+
+        hresp_1 = 1'b1;#1;
+        hresp_2 = 1'b0;#1;
+        hresp_3 = 1'b0;#1;
 
        
     end
